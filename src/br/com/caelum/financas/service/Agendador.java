a76a35a7ee0225e70e91c3890de5166829d35264
anderson-figueiredo <figueiredo.anderson@rocketmail.com>
2017-09-12 16:51:48 -0300

    [agendamento de tarefas]

--- src/br/com/caelum/financas/service/Agendador.java
+++ src/br/com/caelum/financas/service/Agendador.java
@@ -2,8 +2,13 @@ package br.com.caelum.financas.service;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.annotation.Resource;
+import javax.ejb.ScheduleExpression;
 import javax.ejb.Singleton;
-import javax.ejb.Stateless;
+import javax.ejb.Timeout;
+import javax.ejb.Timer;
+import javax.ejb.TimerConfig;
+import javax.ejb.TimerService;
 
 //@Stateless
 @Singleton
@@ -21,7 +26,31 @@ public class Agendador {
 	void preDestruicao(){
 		System.out.println("Destruindo Agendador");
 	}
+	
+	@Resource
+	private TimerService timerService;
+	
+	public void agenda(String expressaoMinutos, String expressaoSegundos) {
+		ScheduleExpression expression = new ScheduleExpression();
+		expression.hour("*");
+		expression.minute(expressaoMinutos);
+		expression.second(expressaoSegundos);
+		
+		TimerConfig config = new TimerConfig();
+		config.setInfo(expression.toString());
+		config.setPersistent(false);
 
+		this.timerService.createCalendarTimer(expression, config);
+		
+		System.out.println("Agendamento: " + expression);
+		
+	}
+
+	@Timeout
+	public void verificacaoPeriodicaSeHaNovasContas(Timer timer) {
+		System.out.println(timer.getInfo());
+	}
+	
 	public void executa() {
 		System.out.printf("%d instancias criadas %n", totalCriado);
 
